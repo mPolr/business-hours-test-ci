5f53ab88ce70bd3aec9d6079777f490b4608fb94
Pavel Monahov <pmonahov@direct-credit.ru>
2021-04-20 08:09:11 +0300

    Add models for 'organization' and 'schedule'

---
+++ app/Models/Schedule.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Schedule extends Model
+{
+	protected $DBGroup              = 'default';
+	protected $table                = 'schedule';
+	protected $primaryKey           = 'id';
+	protected $useAutoIncrement     = true;
+	protected $insertID             = 0;
+	protected $returnType           = 'array';
+	protected $useSoftDelete        = false;
+	protected $protectFields        = true;
+	protected $allowedFields        = ['organization_id', 'day_of_week', 'open', 'close'];
+
+	// Dates
+	protected $useTimestamps        = false;
+	protected $dateFormat           = 'datetime';
+	protected $createdField         = 'created_at';
+	protected $updatedField         = 'updated_at';
+	protected $deletedField         = 'deleted_at';
+
+	// Validation
+	protected $validationRules      = [];
+	protected $validationMessages   = [];
+	protected $skipValidation       = false;
+	protected $cleanValidationRules = true;
+
+	// Callbacks
+	protected $allowCallbacks       = true;
+	protected $beforeInsert         = [];
+	protected $afterInsert          = [];
+	protected $beforeUpdate         = [];
+	protected $afterUpdate          = [];
+	protected $beforeFind           = [];
+	protected $afterFind            = [];
+	protected $beforeDelete         = [];
+	protected $afterDelete          = [];
+}
